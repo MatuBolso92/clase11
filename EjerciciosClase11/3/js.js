cbf90fdc53f51695fbb3f68e9ef5fe22f65b05b6
MatuBolso92 <mfernandez.1992@icloud.com>
2020-08-19 19:45:05 -0300

    se agregó msj

--- EjerciciosClase11/3/js.js
+++ EjerciciosClase11/3/js.js
@@ -1,9 +1,17 @@
 var app = angular.module('agendaApp', []);
 app.controller("agendaCtrl", function($scope) {
-    $scope.tramites = ['Trámite 1', 'Trámite 2', 'Trámite 3', 'Trámite 4'];
+    // $scope.tramites = ['Trámite 1', 'Trámite 2', 'Trámite 3', 'Trámite 4'];
+    let listaTramites = ["Uno", "Dos", "Tres"];
+    $scope.listaTramites = ["Uno", "Dos", "Tres"];
     $scope.tramite;
     $scope.nombreCompleto;
     $scope.edad;
     $scope.fecha;
     $scope.menorEdad = false;
+});
+
+app.controller("ctrlEdad", function($scope){
+    if (menorEdad < 18){
+        menorEdad = true;
+    }
 });
